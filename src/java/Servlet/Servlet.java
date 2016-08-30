2faf1492ad70596bb4392331cb1ef3edf2f9444e
Guilherme <souzasiguilherme@gmail.com>
2016-08-30 18:56:07 -0300

    Criar Aluno, versão 1

--- src/java/Servlet/Servlet.java
+++ src/java/Servlet/Servlet.java
@@ -1,7 +1,13 @@
 package Servlet;
 
+import br.cesjf.lpwsd.dao.AlunoJpaController;
+import classe.Aluno;
 import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.servlet.RequestDispatcher;
@@ -15,32 +21,54 @@ import javax.transaction.UserTransaction;
 @WebServlet(name = "Servlet", urlPatterns = {"/Servlet", "cadastrarAluno.html"})
 public class Servlet extends HttpServlet {
 
-   @PersistenceUnit 
-   (unitName="PDWSDExercicio1PU")
-   EntityManagerFactory emf;
-   @Resource(name="java:comp/UserTransaction")
-   UserTransaction ut;
-    
+    @PersistenceUnit(unitName = "PDWSDExercicio1PU")
+    EntityManagerFactory emf;
+    @Resource(name = "java:comp/UserTransaction")
+    UserTransaction ut;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String _senha=request.getParameter("senha");
-        
-        if(_senha.equals("123")){
-         RequestDispatcher req =request.getRequestDispatcher("/WEB-INF/index.html");
-             req.forward(request, response);
+        String uri = request.getRequestURI();
+        String _senha = request.getParameter("senha");
+
+        if (uri.contains("add")) {
+            request.getRequestDispatcher("/WEB-INF/addAluno.jsp").forward(request, response);
         }
-        else{
-         RequestDispatcher req =request.getRequestDispatcher("/WEB-INF/erro.html");
-             req.forward(request, response);
+
+        if (_senha.equals("123")) {
+            RequestDispatcher req = request.getRequestDispatcher("/WEB-INF/index.html");
+            req.forward(request, response);
+        } else {
+            RequestDispatcher req = request.getRequestDispatcher("/WEB-INF/erro.html");
+            req.forward(request, response);
         }
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-  
+        if (request.getRequestURI().contains("cadastrarAluno.html")) {
+            String nome = request.getParameter("nome");
+            String idade = request.getParameter("idade");
+            //Boolean sexo = Boolean.parseBoolean(request.getParameter("sexo"));
+            Integer matricula = Integer.parseInt(request.getParameter("matricula"));
+            Date inscricao = new Date();
+
+            Aluno a = new Aluno();
+            a.setNome(nome);
+            a.setMatricula(matricula);
+            a.setIdade(idade);
+
+            AlunoJpaController daoAluno = new AlunoJpaController(ut, emf);
+            try {
+                daoAluno.create(a);
+
+                //listAll(request, response);
+            } catch (Exception ex) {
+                Logger.getLogger(Servlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     @Override
